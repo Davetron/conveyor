fe43872800a88963f7b65b03c0ae3db10eb4236a
David Connolly <david.connolly@globalpay.com>
2021-07-02 15:55:25 +0100

    pom updates for publishing to oss sonatype and maven central

---
+++ src/main/java/co/databeast/conveyor/ConveyorExecutor.java
@@ -0,0 +1,18 @@
+/*
+ * Classname : co.databeast.conveyor.ConveyorExecutor
+ *
+ * Created on: 30 Jun 2021
+ *
+ * Copyright (c) 2000-2021 Global Payments, Ltd.
+ * Global Payments, The Observatory, 7-11 Sir John Rogerson's Quay, Dublin 2, Ireland
+ * 
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Global Payments, Ltd. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Global Payments.
+ *
+ */package co.databeast.conveyor;public class ConveyorExecutor {
+}
